88c5fbf074d94c383f24ef40b89f3ecdf2b8de2f
Lucas Graeff Buhl-Nielsen <lucas.graeff-buhl-nielsen21@imperial.ac.uk>
2023-06-15 15:51:36 +0100

    api key

--- lib/src/utils/http_client.dart
+++ lib/src/utils/http_client.dart
@@ -2,11 +2,11 @@ part of 'package:address_search_field/src/services/geo_methods.dart';
 
 Future<Map<String, dynamic>> _httpClient(Uri uri) async {
   final baseOptions = BaseOptions(
-    connectTimeout: 5000,
-    receiveTimeout: 5000,
-    contentType: 'application/json',
-    responseType: ResponseType.json,
-  );
+      connectTimeout: 5000,
+      receiveTimeout: 5000,
+      contentType: 'application/json',
+      responseType: ResponseType.json,
+      headers: {"x-cors-api-key": "temp_cd8e686fa4a52247f5d6dded27fb8a38"});
   final client = Dio(baseOptions);
   final Response<Map<String, dynamic>> response = await client.getUri(uri);
   if (response.statusCode != 200) {
